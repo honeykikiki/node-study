a50408ec5c3f68c8ca5637205325e6700a041c4b
honeykikiki <tjdwls75666@naver.com>
2021-11-16 14:14:56 +0900

    카카오 로그인, 게시물 작성, multer이미지받아오기 학습

--- 9.nodebird/app.js
+++ 9.nodebird/app.js
@@ -10,7 +10,9 @@ const passport = require("passport");
 dotenv.config();
 const pageRouter = require("./routes/page");
 const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
 const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
 app.set("port", process.env.PORT || 8001);
@@ -28,9 +30,11 @@ sequelize
   .catch((err) => {
     console.error(err);
   });
+passportConfig();
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/img", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -51,6 +55,7 @@ app.use(passport.session());
 
 app.use("/", pageRouter);
 app.use("/auth", authRouter);
+app.use("/post", postRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
