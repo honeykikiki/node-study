f76cf9338cf1de884e96e349cc243cc997e3628f
honeykikiki <tjdwls75666@naver.com>
2021-11-20 22:56:44 +0900

    API서버 만들기 sequelize 추가

--- 10.API/nodebird/app.js
+++ 10.API/nodebird/app.js
@@ -17,13 +17,15 @@ const app = express();
 passportConfig();
 app.set("port", process.env.PORT || 8002);
 app.set("view engine", "html");
-nunjucks.configure("views", {
+
+nunjucks.configure("view", {
   express: app,
   watch: true,
 });
 
 sequelize
   .sync({ force: false })
+  // .sync({ alert: true })
   .then(() => {
     console.log("데이터베이스 연결 성공");
   })
